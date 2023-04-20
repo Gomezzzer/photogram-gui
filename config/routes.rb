0380cd123d90e245bc361b5bb3f188dcc059fec3
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2023-04-20 00:24:12 +0000

    Almost done!

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,28 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index" })
 
- get("/", { :controller => "users", :action => "index" })
-
- get("/users", { :controller => "users", :action => "index" })
-
- get("/users/:path_username", { :controller => "users", :action => "show" })
-
- get("/insert_user", { :controller => "users", :action => "create" })
-
- get("/update_user", { :controller => "users", :action => "create" })
-
-
- get("/photos", { :controller => "photos", :action => "index" })
-
- get("/photos/:path_id", { :controller => "photos", :action => "show" })
-
- get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii" })
-
- get("/insert_photo", { :controller => "photos", :action => "create" })
-
- get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
-
- get("/insert_user_record", { :controller => "users", :action => "create" })
-
- get("/insert_comment_record", { :controller => "photos", :action => "comment" })
+  get("/users", { :controller => "users", :action => "index" })
+ 
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+ 
+  get("/insert_user", { :controller => "users", :action => "create" })
+ 
+  get("/update_user", { :controller => "users", :action => "create" })
+ 
+ 
+  get("/photos", { :controller => "photos", :action => "index" })
+ 
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+ 
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii" })
+ 
+  get("/insert_photo", { :controller => "photos", :action => "create" })
+ 
+  get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
+ 
+  get("/insert_user_record", { :controller => "users", :action => "create" })
+ 
+  get("/insert_comment_record", { :controller => "photos", :action => "comment" })
 
  
 
